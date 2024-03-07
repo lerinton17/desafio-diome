b158f5c375c4acf94e16cd808b437167f9fd87d7
lerinton17 <162178221+lerinton17@users.noreply.github.com>
2024-03-07 15:28:01 -0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("teste github")
+console.log("Bem-vindo")
+
+let heroi = "lerinton17"
+let xpLvl = 10001
+
+if (xpLvl<1000) {
+    xpLvl = "ferro"
+} else if(xpLvl<2000) {
+    xpLvl = "bronze"
+} else if (xpLvl<5000) {
+    xpLvl = "prata"
+} else if (xpLvl<8000) {
+    xpLvl = "ouro"
+} else if (xpLvl<9000) {
+    xpLvl = "ascendente"
+} else if (xpLvl<10000) {
+    xpLvl = "imortal"
+} else{
+    xpLvl= "radiante"
+}
+
+console.log("O herói "+ heroi + " está no nível "+ xpLvl)
